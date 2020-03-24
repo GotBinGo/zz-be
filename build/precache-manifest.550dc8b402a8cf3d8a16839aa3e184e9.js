2caecbeade0928c4b5be832663f7264800599e93
Tamas Bordak <tamas@webtar.hu>
2020-03-24 11:47:39 +0100

    init

---
+++ build/precache-manifest.550dc8b402a8cf3d8a16839aa3e184e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ca95356a6ac837c38b2a74da1d4eeb0",
+    "url": "/static/media/zalazone.7ca95356.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "08613accb026bf986004",
+    "url": "/static/js/main.ba48af2c.chunk.js"
+  },
+  {
+    "revision": "f3c856593560188347de",
+    "url": "/static/js/2.76477ae5.chunk.js"
+  },
+  {
+    "revision": "08613accb026bf986004",
+    "url": "/static/css/main.75b9d70e.chunk.css"
+  },
+  {
+    "revision": "f3c856593560188347de",
+    "url": "/static/css/2.88fcde52.chunk.css"
+  },
+  {
+    "revision": "a9592424f5c373e078b61ed33147a770",
+    "url": "/index.html"
+  }
+];
---
+++ server.js
@@ -0,0 +1,37 @@
+'use strict';
+const express = require('express');
+const bodyParser = require('body-parser');
+var request = require('request');
+
+const app = express();
+const url = '/data';
+const users = require('./users.json');
+
+app.use(bodyParser.json());
+app.use('/', express.static('build'))
+
+app.get(url, function (req, res) {
+
+    var username = "username";
+    var password = "password";
+    var auth = "Basic " + new Buffer(username + ":" + password).toString("base64");
+    var url = "https://0f86eebe-a798-4ec5-969b-a8c5fcec4f85.eu10.cp.iot.sap/iot/processing/api/v1/tenant/43294285/measures/capabilities/d0befbd3-2e30-4671-baff-76f0a52bcb12?skip=0&top=100";
+
+    request.get({
+        url : url,
+        headers : {
+            "Authorization" : auth
+        }
+        }, function(error, response, body) {
+        res.status(200).json(JSON.parse(body));
+    } );
+});
+
+const PORT = process.env.PORT || 8088;
+
+var server = app.listen(PORT, function () {
+
+    const host = server.address().address;
+    const port = server.address().port;
+
+});

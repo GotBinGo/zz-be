f9aa9ca550de84a2cf3cb0ebdc180dbe80864280
Kovács Dániel <daniel.kovacs314@gmail.com>
2020-03-31 11:44:38 +0200

    Refact

--- zz-fe/src/app/sensor/sensor.component.ts
+++ zz-fe/src/app/sensor/sensor.component.ts
@@ -10,11 +10,11 @@ import { DataService } from '../data.service';
 })
 export class SensorComponent implements OnInit {
   sensorModel = [
-    new SensorTableModel("sensor1", 24, new Date(), true, 320, 550, 'rgb(255,99,132)'),
-    new SensorTableModel("sensor2", 24.5, new Date(), true, 360, 575, 'rgb(54,162,235)'),
-    new SensorTableModel("sensor3", 23.5, new Date(), true, 410, 560, 'rgb(255,206,86)'),
-    new SensorTableModel("sensor4", 22, new Date(), true, 420, 510, 'rgb(231,233,237)'),
-    new SensorTableModel("sensor5", 23.2, new Date(), true, 430, 450, 'rgb(75,192,192)')
+    new SensorTableModel("sensor1", 24, new Date(), true, 320, 550, 10, 'rgb(255,99,132)'),
+    new SensorTableModel("sensor2", 24.5, new Date(), true, 360, 575, 10, 'rgb(54,162,235)'),
+    new SensorTableModel("sensor3", 23.5, new Date(), true, 410, 560, 10, 'rgb(255,206,86)'),
+    new SensorTableModel("sensor4", 22, new Date(), true, 420, 510, 10, 'rgb(231,233,237)'),
+    new SensorTableModel("sensor5", 23.2, new Date(), true, 430, 450, 10, 'rgb(75,192,192)')
   ];
 
   lineChartData: ChartDataSets[] = [
@@ -82,7 +82,36 @@ export class SensorComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.draw();
+
+    this.canvas.nativeElement.addEventListener('click', (e) => {
+      const color = this.getColor(e);
+
+      let isUnCheckAll = true;
+      this.sensorModel.forEach(sensor => {
+        if (color == sensor.color) {
+          this.lineChartData.forEach(l => l.hidden = l.label != sensor.id);
+          sensor.radius = 15;
+          this.selectedSensorId = sensor.id;
+          isUnCheckAll = false;
+        } else {
+          sensor.radius = 10;
+        }
+      });
+
+      if(isUnCheckAll){
+        this.lineChartData.forEach(l => l.hidden = false);
+        this.selectedSensorId = "";
+      }
+
+      this.draw();
+      this.chart.update();
+    });
+  }
+
+  private draw() {
     this.ctx = this.canvas.nativeElement.getContext('2d');
+    this.ctx.clearRect(0, 0, 600, 800);
 
     const mapImg = new Image();
     mapImg.src = "assets/image/DP-1.jpg";
@@ -91,36 +120,25 @@ export class SensorComponent implements OnInit {
       this.ctx.drawImage(mapImg, 0, 0, mapImg.width * 0.25, mapImg.height * 0.25);
       this.sensorModel.forEach(s => {
         this.ctx.beginPath();
-        this.ctx.arc(s.x, s.y, 10, 0, 2 * Math.PI);
+        console.log(s.radius);
+        this.ctx.arc(s.x, s.y, s.radius, 0, 2 * Math.PI);
         this.ctx.fillStyle = s.color;
         this.ctx.fill();
       });
     };
+  }
 
-    this.canvas.nativeElement.addEventListener('click', (e) => {
-      let x = e.pageX - this.canvas.nativeElement.getBoundingClientRect().left - document.documentElement.scrollLeft;
-      let y = e.pageY - this.canvas.nativeElement.getBoundingClientRect().top - document.documentElement.scrollTop;
+  private getMousePosition(e: MouseEvent): { x: number, y: number } {
+    return {
+      x: e.pageX - this.canvas.nativeElement.getBoundingClientRect().left - document.documentElement.scrollLeft,
+      y: e.pageY - this.canvas.nativeElement.getBoundingClientRect().top - document.documentElement.scrollTop
+    };
+  }
 
-
-      console.log(x, y)
-
-      const mousePos = {
-        x: x,
-        y: y
-      };
-
-      const pixel = this.ctx.getImageData(mousePos.x, mousePos.y, 1, 1).data;
-
-      const color = `rgb(${pixel[0]},${pixel[1]},${pixel[2]})`;
-
-      this.sensorModel.forEach(sensor => {
-        if (color == sensor.color) {
-          this.lineChartData.forEach(l => l.hidden = l.label != sensor.id);
-          this.selectedSensorId = sensor.id;
-          this.chart.update();
-        }
-      });
-    });
+  private getColor(e: MouseEvent): string {
+    const mousePos = this.getMousePosition(e);
+    const pixel = this.ctx.getImageData(mousePos.x, mousePos.y, 1, 1).data;
+    return `rgb(${pixel[0]},${pixel[1]},${pixel[2]})`;
   }
 
 }
@@ -133,6 +151,7 @@ export class SensorTableModel {
     public active: boolean,
     public x: number,
     public y: number,
+    public radius: number,
     public color: string
   ) {
   }
